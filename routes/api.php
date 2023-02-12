fb7c7e4455bf2435b1b4577d397f6e4656e9724d
Asset1996 <khaitbay96@gmail.com>
2023-02-12 21:16:06 +0500

    Laratrust installed

--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
-
     Route::post('login', [AuthController::class, 'authenticate']);
-
     Route::get('only-auth', [AuthController::class, 'onlyAuth'])->middleware('authenticated');
-    //test
-
 });
